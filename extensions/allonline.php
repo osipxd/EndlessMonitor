0c684d9146dad362db063dfbbbb8afaf54ccd934
Osip Fatkullin <osip.fatkullin@gmail.com>
2013-02-24 21:50:30 +0400

    Ext - AllOnline
    Общий онлайн

--- extensions/allonline.php
+++ extensions/allonline.php
@@ -1,18 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<meta name="author" content="OsipXD" />
+<meta name="copyright" content="EndlessWorlds" />
+<meta charset="utf-8">
+
+<?php require '../config.php';  
+
+if ($debug) {
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);    
+}
+?>
+<style>
+    @font-face {               
+        font-family: Style-Font;
+        src: url(<?php echo '..',$style_path,'/',$style,'/font.ttf'; ?>);
+    }
+    p {
+        font-family: Style-Font, serif, cursive;
+        font-size: 14px; /* Тут менять размер шрифта */
+    }
+</style>
 <?php
-require '../funcs.php';
+// Получаем информацию с серверов
+function get_res($address,$port) {    
+    $socket = @fsockopen($address,$port);
+    if ($socket == false)  $online = 0; 
+    else {
+        @fwrite($socket, "\xFE");
+        $cash = "";
+        $cash = @fread($socket, 1024);
+        @fclose($socket);
+        if ($cash !== false && substr($cash, 0, 1) == "\xFF") {
+            $info = explode("\xA7", mb_convert_encoding(substr($cash,1), 'iso-8859-1', 'utf-16be'));
+            $online = $info[1];
+        } else $online = 0;
+    }
+    return $online;
+}
 
 $max = 0;
-$realonline = 0;
-$align = center;
+$real = 0;
 
-foreach ($servers as $servername) {
-    $ip = $ips[$servername];
-    $port = $ports[$servername];
-    $max += $maxonline[$servername];
-    $res += get_res($ip,$port);
-    if (isset($res['report'])) $res['online'] = 0; 
-    $realonline .= $res['online'];
-} 
-    
-$img = imagecreatefrompng(dirname(__FILE__).'/extensions/allonline.png');                         
-?>
+$last_time = file_get_contents('cron.txt');
+$period = time() - 60 * $interval;
+if ($last_time < $period || $debug) {
+    foreach ($servers as $servername) {
+        $ip = $ips[$servername];
+        $port = $ports[$servername];
+        $max += $maxonline[$servername]; 
+        $real += get_res($ip,$port);
+        file_put_contents('last.txt', $real.'/'.$max);
+        file_put_contents('cron.txt', time());
+    }
+}
+$all = file_get_contents('last.txt');
+
+if ($debug) {
+    ini_set('display_errors', 0);
+    error_reporting(0);    
+}
+?> 
+<p> Общий онлайн: <?php echo $all; ?> </p>
+</html>
